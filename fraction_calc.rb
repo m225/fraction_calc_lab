305c6dd708a4ff47193cebfbe2cc5e72bbb90ec7
Morgan Rizy <mr49748@students.westport.k12.ct.us>
2014-11-05 11:14:06 -0500

    Initial Commit

--- fraction_calc.rb
+++ fraction_calc.rb
@@ -4,9 +4,8 @@ puts "Press the enter key to run the program."
 def multiply(n1, d1, n2, d2)
   nn = (n1*n2)
   nd = (d1*d2)
- simplify(nn, nd)
-[nn, nd]
-end
+  [nn, nd]
+  end
 
 def add(n1, d1, n2, d2)
   lcm = d1.lcm(d2)
@@ -15,8 +14,7 @@ def add(n1, d1, n2, d2)
    nd1 = lcm
    nd2 = lcm
   finalnumerator = newnum1 + newnum2
-simplify(finalnumerator, lcm) 
-[finalnumerator, lcm]
+ [finalnumerator, lcm]
 end
 
 def subtract(n1, d1, n2, d2)
@@ -53,7 +51,7 @@ n1, d1 = gets.split('/').collect(& :to_i)
 
 # (nn, nd) it is returned in an array
 
-output_fraction(multiply(2, 2, 1, 2))
+output_fraction(multiply(2, 4, 2, 4))
 output_fraction(add(2, 4, 3, 6))
 output_fraction(subtract(1, 2, 1, 4))
 output_fraction(divide(4, 5, 2, 3))
